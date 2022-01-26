aaefe1ae6f1353123b0745be35fd67648dfc4ca8
Vijay Pillai <vijay.pillai397@gmail.com>
2022-01-26 10:47:20 -0500

    remove windows support from bouncer.sh and also add linux/arm64 and darwin/arm64 to list of supported platforms (#3)
    Signed-off-by: Vijay Pillai <vijay.pillai@anchore.com>

--- bouncer.sh
+++ bouncer.sh
@@ -64,7 +64,8 @@ get_binaries() {
   case "$PLATFORM" in
     darwin/amd64) BINARIES="bouncer" ;;
     linux/amd64) BINARIES="bouncer" ;;
-    windows/amd64) BINARIES="bouncer" ;;
+    darwin/arm64) BINARIES="bouncer" ;;
+    linux/arm64) BINARIES="bouncer" ;;
     *)
       log_crit "platform $PLATFORM is not supported.  Make sure this script is up-to-date and file request at https://github.com/${PREFIX}/issues/new"
       exit 1
